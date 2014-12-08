b4617ad923c4c1ac08798e9c1c7a2463839d540e
Dex Bleeker <dex@bleeker.nl>
2014-12-09 00:25:04 +0100

    Werkt niet

--- src/com/ecci/Hamers/MainActivity.java
+++ src/com/ecci/Hamers/MainActivity.java
@@ -2,11 +2,20 @@ package com.ecci.Hamers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+    // Drawer list
+    private String[] mDrawerItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+
     /**
      * Called when the activity is first created.
      */
@@ -15,11 +24,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // Test Button
         Button button = (Button) findViewById(R.id.test_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
             }
         });
+
+        // Drawer list
+        mDrawerItems = getResources().getStringArray(R.array.drawer_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.main, mDrawerItems));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 }
