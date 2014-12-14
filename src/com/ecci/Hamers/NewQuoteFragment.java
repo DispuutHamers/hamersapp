cc26a8bfef3fae403ddb63ba05c8e0c197455ea7
Dex Bleeker <dex@bleeker.nl>
2014-12-14 01:21:18 +0100

    Spinner werkt

--- src/com/ecci/Hamers/NewQuoteFragment.java
+++ src/com/ecci/Hamers/NewQuoteFragment.java
@@ -6,6 +6,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewQuoteFragment extends DialogFragment {
 
@@ -16,7 +19,8 @@ public class NewQuoteFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.new_quote_fragment, null))
+        View view = inflater.inflate(R.layout.new_quote_fragment, null);
+        builder.setView(view)
                 .setTitle(R.string.quote)
                 .setPositiveButton(R.string.send_quote, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
@@ -29,7 +33,18 @@ public class NewQuoteFragment extends DialogFragment {
                     }
                 });
 
+        // Initialize spinner
+        Spinner spinner = (Spinner) view.findViewById(R.id.user_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
+                R.array.users_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         // Create the AlertDialog object and return it
         return builder.create();
     }
+
 }
