48e6ffa86875ff38fa017ca5943966b30ace29ff
Dex Bleeker <dex@bleeker.nl>
2015-02-16 03:46:47 +0100

    Sorting users

--- src/nl/ecci/Hamers/Users/UserFragment.java
+++ src/nl/ecci/Hamers/Users/UserFragment.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import nl.ecci.Hamers.Beers.Beer;
 import nl.ecci.Hamers.Helpers.DataManager;
 import nl.ecci.Hamers.Helpers.GetJson;
 import nl.ecci.Hamers.R;
@@ -20,12 +19,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class UserFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     ArrayList<User> listItems = new ArrayList<User>();
     ArrayAdapter<User> adapter;
     SwipeRefreshLayout swipeView;
+    SharedPreferences prefs;
 
     public UserFragment() {
         // Empty constructor required for fragment subclasses
@@ -36,10 +38,14 @@ public class UserFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         View view = inflater.inflate(R.layout.user_fragment, container, false);
         ListView user_list = (ListView) view.findViewById(R.id.users_listView);
 
+        setHasOptionsMenu(true);
+
         initSwiper(view, user_list);
 
         adapter = new UsersAdapter(this.getActivity(), listItems);
         user_list.setAdapter(adapter);
+
+        sort();
 
         return view;
     }
@@ -73,6 +79,11 @@ public class UserFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.user_list_menu, menu);
+    }
+
+    @Override
     public void onRefresh() {
         GetJson g = new GetJson(this.getActivity(), this, GetJson.USERURL, PreferenceManager.getDefaultSharedPreferences(this.getActivity()), false);
         g.execute();
@@ -100,4 +111,81 @@ public class UserFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             swipeView.setRefreshing(false);
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_username:
+                sortByUsername();
+                return true;
+            case R.id.sort_quotes:
+                sortbyQuoteCount();
+                return true;
+            case R.id.sort_reviews:
+                sortbyReviewCount();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void sort() {
+        prefs = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+        String sortPref = prefs.getString("userSort", "");
+        if (sortPref.equals("name")) {
+            sortByUsername();
+        } else if (sortPref.equals("quotecount")) {
+            sortbyQuoteCount();
+        } else if (sortPref.equals("reviewcount")) {
+            sortbyReviewCount();
+        }
+    }
+
+    public void sortByUsername() {
+        Collections.sort(listItems, nameComperator);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void sortbyQuoteCount() {
+        Collections.sort(listItems, quoteComperator);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void sortbyReviewCount() {
+        Collections.sort(listItems, reviewComperator);
+        adapter.notifyDataSetChanged();
+    }
+
+    public Comparator<User> nameComperator = new Comparator<User>() {
+        @Override
+        public int compare(User user1, User user2) {
+
+            String name1 = user1.getUsername();
+            String name2 = user2.getUsername();
+
+            return name1.compareToIgnoreCase(name2);
+        }
+    };
+
+    public Comparator<User> quoteComperator = new Comparator<User>() {
+        @Override
+        public int compare(User user1, User user2) {
+
+            int quote1 = user1.getQuotecount();
+            int quote2 = user2.getQuotecount();
+
+            return ((Integer)quote2).compareTo(quote1);
+        }
+    };
+
+    public Comparator<User> reviewComperator = new Comparator<User>() {
+        @Override
+        public int compare(User user1, User user2) {
+
+            int review1 = user1.getReviewcount();
+            int review2 = user2.getReviewcount();
+
+            return ((Integer)review2).compareTo(review1);
+        }
+    };
 }
