e553ef874a4e9fd7736908f6bec24b2b7b9e563b
Dex Bleeker <dex@bleeker.nl>
2015-07-02 17:22:37 +0200

    Event detailed redesign

--- hamersapp/src/main/java/nl/ecci/hamers/events/SingleEventActivity.java
+++ hamersapp/src/main/java/nl/ecci/hamers/events/SingleEventActivity.java
@@ -1,14 +1,19 @@
 package nl.ecci.hamers.events;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 import nl.ecci.hamers.R;
 import nl.ecci.hamers.helpers.DataManager;
@@ -19,6 +24,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 public class SingleEventActivity extends AppCompatActivity {
 
@@ -31,6 +37,7 @@ public class SingleEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.single_event);
 
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         parentLayout = (LinearLayout) findViewById(R.id.single_event_parent);
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -43,37 +50,48 @@ public class SingleEventActivity extends AppCompatActivity {
             actionBar.setHomeButtonEnabled(true);
         }
 
-        ScrollView scrollView = (ScrollView) findViewById(R.id.single_event_scrollview);
         LinearLayout buttonLayout = (LinearLayout) findViewById(R.id.buttonLayout);
         Button aanwezigButton = (Button) findViewById(R.id.aanwezig_button);
         Button afwezigButton = (Button) findViewById(R.id.afwezig_button);
 
         TextView titleTV = (TextView) findViewById(R.id.event_title);
-        TextView beschrijvingTV = (TextView) findViewById(R.id.event_beschrijving);
-        TextView dateTV = (TextView) findViewById(R.id.event_date);
-        TextView locationTV = (TextView) findViewById(R.id.event_location);
-        ListView aanwezig_list = (ListView) findViewById(R.id.event_aanwezig);
-        ListView afwezig_list = (ListView) findViewById(R.id.event_afwezig);
+
+        View dateRow = findViewById(R.id.row_date);
+        View locationRow = findViewById(R.id.row_location);
+        View descriptionRow = findViewById(R.id.row_description);
+
         LinearLayout aanwezig_layout = (LinearLayout) findViewById(R.id.aanwezig_layout);
+
+        ViewGroup aanwezigView = (ViewGroup) findViewById(R.id.aanwezig_insert_point);
+        ViewGroup afwezigView = (ViewGroup) findViewById(R.id.afwezig_insert_point);
 
         Bundle extras = getIntent().getExtras();
         id = extras.getInt("id");
         ArrayList<String> aanwezigItems = getIntent().getStringArrayListExtra("aanwezig");
         ArrayList<String> afwezigItems = getIntent().getStringArrayListExtra("afwezig");
 
-        ArrayAdapter<String> aanwezigAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, aanwezigItems);
-        ArrayAdapter<String> afwezigAadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, afwezigItems);
-        aanwezig_list.setAdapter(aanwezigAdapter);
-        afwezig_list.setAdapter(afwezigAadapter);
+        if (aanwezigItems.size() != 0) {
+            for(String u : aanwezigItems) {
+                View view = inflater.inflate(R.layout.row_singleview, null);
 
-        // Remove headers if no user is enrolled for that category
-        if (aanwezigItems.size() == 0) {
-            TextView event_aanwezig_tv = (TextView) findViewById(R.id.event_aanwezig_tv);
-            event_aanwezig_tv.setVisibility(View.INVISIBLE);
+                fillSingleRow(view, u);
+
+                aanwezigView.addView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            }
+        } else {
+            aanwezigView.removeAllViews();
         }
-        if (afwezigItems.size() == 0) {
-            TextView event_afwezig_tv = (TextView) findViewById(R.id.event_afwezig_tv);
-            event_afwezig_tv.setVisibility(View.INVISIBLE);
+
+        if (afwezigItems.size() != 0) {
+            for(String u : afwezigItems) {
+                View view = inflater.inflate(R.layout.row_singleview, null);
+
+                fillSingleRow(view, u);
+
+                afwezigView.addView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            }
+        } else {
+            afwezigView.removeAllViews();
         }
 
         String title = extras.getString("title");
@@ -87,27 +105,29 @@ public class SingleEventActivity extends AppCompatActivity {
 
             // Event date
             String date = extras.getString("date");
-            DateFormat dbDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-            DateFormat appDF = new SimpleDateFormat("EEE dd MMM yyyy HH:mm");
+            DateFormat dbDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", new Locale("nl"));
+            DateFormat appDF = new SimpleDateFormat("EEE dd MMM yyyy HH:mm", new Locale("nl"));
             Date dbDatum = dbDF.parse(date);
             appDatum = appDF.format(dbDatum);
 
             if (today.after(dbDatum)) {
                 aanwezig_layout.setVisibility(View.GONE);
+                ScrollView scrollView = (ScrollView) findViewById(R.id.single_event_scrollview);
                 scrollView.removeView(buttonLayout);
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
+        Resources res = getResources();
         titleTV.setText(title);
-        beschrijvingTV.setText(beschrijving);
-        dateTV.setText(appDatum);
+        fillDetailRow(descriptionRow, "Beschrijving", beschrijving);
+        fillImageRow(dateRow, "Datum", appDatum, res.getDrawable(R.drawable.ic_event));
 
         if (!location.equals("null") && !location.equals("")) {
-            locationTV.setText(location);
+            fillImageRow(locationRow, "Locatie", location, res.getDrawable(R.drawable.ic_location));
         } else {
-            locationTV.setVisibility(View.GONE);
+            locationRow.setVisibility(View.GONE);
         }
 
         String userName = DataManager.getUserName(prefs);
@@ -156,5 +176,29 @@ public class SingleEventActivity extends AppCompatActivity {
     @Override
     public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    public void fillSingleRow(View view, final String title) {
+        TextView titleView = (TextView) view.findViewById(R.id.title);
+        titleView.setText(title);
+    }
+
+    public void fillDetailRow(View view, final String title, final String description) {
+        TextView titleView = (TextView) view.findViewById(R.id.title);
+        titleView.setText(title);
+
+        TextView descriptionView = (TextView) view.findViewById(R.id.description);
+        descriptionView.setText(description);
+    }
+
+    public void fillImageRow(View view, final String title, final String description, final Drawable image) {
+        TextView titleView = (TextView) view.findViewById(R.id.title);
+        titleView.setText(title);
+
+        TextView descriptionView = (TextView) view.findViewById(R.id.description);
+        descriptionView.setText(description);
+
+        ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+        iconView.setImageDrawable(image);
     }
 }
