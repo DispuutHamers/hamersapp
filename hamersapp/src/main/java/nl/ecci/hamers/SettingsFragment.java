773e0231a27ab73485d875a192959ca4b0c08d95
Dex Bleeker <dex@bleeker.nl>
2016-05-09 17:02:43 +0200

    Button (in preferences) to clear image cache

--- hamersapp/src/main/java/nl/ecci/hamers/SettingsFragment.java
+++ hamersapp/src/main/java/nl/ecci/hamers/SettingsFragment.java
@@ -1,12 +1,34 @@
 package nl.ecci.hamers;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.View;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference clear_image_cache = findPreference("clear_image_cache");
+        clear_image_cache.setOnPreferenceClickListener(
+                new OnPreferenceClickListener() {
+                    public boolean onPreferenceClick(Preference preference) {
+                        ImageLoader.getInstance().clearMemoryCache();
+                        ImageLoader.getInstance().clearDiskCache();
+
+                        View view = getView();
+                        if (view != null) {
+                            Snackbar.make(view, R.string.clear_image_cache_snackbar, Snackbar.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                }
+        );
     }
 }
