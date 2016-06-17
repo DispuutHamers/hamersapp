c1bb9fdc988efd7e8c352856d70f5a97be8ae2f1
Dex Bleeker <dex@bleeker.nl>
2016-06-17 19:29:33 +0200

    Merge NewNewsActivity

--- hamersapp/src/main/java/nl/ecci/hamers/news/NewNewsActivity.java
+++ hamersapp/src/main/java/nl/ecci/hamers/news/NewNewsActivity.java
@@ -1,8 +1,6 @@
 package nl.ecci.hamers.news;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
