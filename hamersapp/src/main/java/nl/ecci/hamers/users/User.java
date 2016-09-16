ed70d4dd2f652c1576ac07bd9ce9735c65dca3d2
Dex Bleeker <dex@bleeker.nl>
2016-09-16 15:46:53 +0200

    Consistent names for user types

--- hamersapp/src/main/java/nl/ecci/hamers/users/User.java
+++ hamersapp/src/main/java/nl/ecci/hamers/users/User.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class User {
 
     public enum Member {
-        @SerializedName("Lid")
+        @SerializedName("lid")
         LID,
         @SerializedName("a-lid")
         ALID,
