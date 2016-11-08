ef41908f7dba106e087ca5a33c1ef7ac5e6bf3da
Dex Bleeker <dex@bleeker.nl>
2016-11-08 21:13:20 +0100

    Replace send button in NewBeerActivity with animated button in toolbar

--- hamersapp/src/main/java/nl/ecci/hamers/beers/NewBeerActivity.java
+++ hamersapp/src/main/java/nl/ecci/hamers/beers/NewBeerActivity.java
@@ -1,14 +1,22 @@
 package nl.ecci.hamers.beers;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.android.volley.VolleyError;
 
@@ -33,6 +41,7 @@ public class NewBeerActivity extends HamersActivity {
     private EditText beerPercentage;
     private EditText beerBrewer;
     private EditText beerCountry;
+    private MenuItem refreshItem;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,7 +80,36 @@ public class NewBeerActivity extends HamersActivity {
         }
     }
 
-    public void postBeer(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.new_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.done_button:
+                refreshItem = item;
+
+                /* Attach a rotating ImageView to the refresh item as an ActionView */
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                ImageView iv = (ImageView) inflater.inflate(R.layout.refresh_action_view, null);
+
+                Animation rotation = AnimationUtils.loadAnimation(this, R.anim.clockwise_refresh);
+                rotation.setRepeatCount(Animation.INFINITE);
+                iv.startAnimation(rotation);
+
+                refreshItem.setActionView(iv);
+
+                postBeer();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void postBeer() {
         String percentage = beerPercentage.getText().toString();
 
         if (!percentage.contains("%")) {
@@ -104,7 +142,10 @@ public class NewBeerActivity extends HamersActivity {
 
             @Override
             public void onError(VolleyError error) {
-
+                if (refreshItem != null && refreshItem.getActionView() != null) {
+                    refreshItem.getActionView().clearAnimation();
+                    refreshItem.setActionView(null);
+                }
             }
         });
     }
