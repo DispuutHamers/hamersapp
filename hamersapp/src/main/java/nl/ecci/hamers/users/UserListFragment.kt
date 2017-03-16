50bcdca295d4e4532dd0d6124bf2f7f3dca87207
Dex Bleeker <dex@bleeker.nl>
2017-03-16 15:08:39 +0100

    Notify adapter after sorting users

--- hamersapp/src/main/java/nl/ecci/hamers/users/UserListFragment.kt
+++ hamersapp/src/main/java/nl/ecci/hamers/users/UserListFragment.kt
@@ -98,21 +98,25 @@ class UserListFragment : HamersListFragment(), SwipeRefreshLayout.OnRefreshListe
     private fun sortByUsername() {
         val nameComperator = Comparator<User> { user1, user2 -> user1.name.compareTo(user2.name, ignoreCase = true) }
         Collections.sort(dataSet, nameComperator)
+        notifyAdapter()
     }
 
     private fun sortByQuoteCount() {
         val quoteComperator = Comparator<User> { user1, user2 -> user2.quoteCount - user1.quoteCount }
         Collections.sort(dataSet, quoteComperator)
+        notifyAdapter()
     }
 
     private fun sortByReviewCount() {
         val reviewComperator = Comparator<User> { user1, user2 -> user2.reviewCount - user1.reviewCount }
         Collections.sort(dataSet, reviewComperator)
+        notifyAdapter()
     }
 
     private fun sortByBatch() {
         val batchComperator = Comparator<User> { user1, user2 -> user1.batch - user2.batch }
         Collections.sort(dataSet, batchComperator)
+        notifyAdapter()
     }
 
     private inner class populateList : AsyncTask<String, Void, ArrayList<User>>() {
